d430a31b9e29f3c524c9d78b550d7b0944928af8
Bram Altunian <baaltunian@yahoo.com>
2020-05-07 17:51:08 -0400

    Med difficulty added

--- Game.py
+++ Game.py
@@ -271,6 +271,320 @@ def easyDifficulty():
 
 	print("Thank you for playing. Goodbye")
 
+def mediumDifficulty():
+	points = 0
+	penalties = 0
+	answer_choices = ['A','a','B','b','C','c','D','d']
+
+	print('''You have selected the medium difficulty. Things will be different here.
+			 You will once again be asked multiple choice questions, however, there is one caveat.
+			 You will be penalized for every missed question and will have a limit of three penalties.
+			 After three missed questions, you lose. Good luck.''')
+
+	while penalties < 3:
+		print('''Question 1: Who created the Belgian detective Hercule Poirot?
+				A) Sir Arthur Conan Doyle
+				B) Agatha Christie
+				C) Robert Ludlum
+				D) Ian Fleming ''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'B' or answer == 'b':
+			print("Correct")
+			points+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+
+		print('''Question 2: Which European city is home to the Trevi Fountain?
+				A) Rome
+				B) Bern
+				C) Naples
+				D) Berlin ''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Correct")
+			points+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+
+		print('''Question 3: What country does the dish Goulash originally come from?
+				A) Hungary
+				B) Russia
+				C) Belarus
+				D) Ukraine''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Correct")
+			points+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question 4: How many carats is pure gold?
+				A) 24 carats
+				B) 21 carats
+				C) 15 carats
+				D) 32 carats''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Correct")
+			points+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question 5: In traditional occupations, what did a cooper make?
+				A) Barrels
+				B) Shoes
+				C) Coops
+				D) Crafts''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Correct")
+			points+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question 6: In traditional occupations, what did a cobbler make? 
+				A) Barrels
+				B) Shoes
+				C) Coops
+				D) Crafts''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'B' or answer == 'b':
+			print("Correct")
+			points+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break;
+
+		print('''Question 7: What was Elton John's first U.S. Number 1 hit?
+				A) Crocodile Rock
+				B) Your Song
+				C) Goodbye Yellow Brick Road
+				D) Candle in the Wind''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Correct")
+			points+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question 8: Which group made the albums Bare Trees and Penguin?
+				A) The Moody Blues
+				B) Cream
+				C) Fleetwood Mac
+				D) Santana''')
+
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'B' or answer == 'b':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'C' or answer == 'c':
+			print("Correct")
+			points+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question  9: Where in the U.K. is actor Liam Neeson from?
+				A) England
+				B) Northern Ireland
+				C) Scotland
+				D) Wales''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Incorrect")
+			penalties+=1
+		elif answer == 'B' or answer == 'b':
+			print("Correct")
+			points+=1
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print("Total points: {}  Total penalties: {}".format(points,penalties))
+		if penalties == 3:
+			break
+
+		print('''Question 10 will be slightly more difficult than the previous nine and
+				will thus be worth more. Question 10 will be worth 3 points''')
+		print('''Question 10: All the President's Men, released in 1976 and based on the
+				book of the same name, deatiled the Watergate Scandal which brought an end
+				to Richard Nixon's presidency. This landmark political thriller starred
+				Robert Redford and what other 1970's acclaimed actor?
+				A) Al Pacino
+				B) Dustin Hoffman
+				C) Gene Hackman
+				D) Jack Nicholson''')
+		answer = input()
+
+		while answer not in answer_choices:
+			print("That is an invalid choice. Please enter A, B, C, or D.")
+			answer = input()
+
+		if answer == 'A' or answer == 'a':
+			print("Incorrect")
+			penalties+=1 
+		elif answer == 'B' or answer == 'b':
+			print("Correct")
+			points+=3
+		elif answer == 'C' or answer == 'c':
+			print("Incorrect")
+			penalties+=1
+		else:
+			print("Incorrect")
+			penalties+=1
+
+		print('''Calculating final score...
+				...
+				...
+				...
+				Final score is {}'''.format(points))
+
+		if penalties == 3:
+			break
+		else:
+			if points == 12:
+				print("You got a perfect score!")
+			elif points > 10:
+				print("You scored great!")
+			elif points > 6:
+				print("You did a pretty good job")
+			else:
+				print("Better luck next time")
+
+		print("Thank you for playing. Goodbye")
+		break
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -283,6 +597,9 @@ if __name__ == '__main__':
 
 	if difficulty == "Easy":
 		easyDifficulty()
+
+	elif difficulty == "Medium":
+		mediumDifficulty()
 
 
 
